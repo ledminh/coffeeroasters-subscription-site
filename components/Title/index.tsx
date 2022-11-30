227e180a3d162a1ef8f44ae997dd064aec41b60a
Minh Le <duyminh.le310@gmail.com>
2022-11-30 16:49:05 -0500

    Logo

--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -1,8 +1,21 @@
 import { FunctionComponent } from "react";
 
+import Image from "next/image";
+import styles from './Title.module.scss';
+
+import logoSVG from '../../assets/images/shared/desktop/logo.svg';
+
 const Title:FunctionComponent = () => {
 
-    return <>This is title</>
+    return (
+        <div className={styles.wrapper}>
+            <Image 
+                src={logoSVG}
+                alt="coffeeroasters logo"
+                fill={true}
+            />
+        </div>
+    )
 }
 
 export default Title;
