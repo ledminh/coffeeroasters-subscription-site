799da657e74e83cda8e97f1b9cf9469f1d34d3f7
Minh Le <duyminh.le310@gmail.com>
2022-11-30 18:16:14 -0500

    NavBar

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,8 +1,23 @@
 import { FunctionComponent } from "react";
 
+import styles from './NavBar.module.scss';
+import Image from "next/image";
+import iconHamburgerSVG from '../../assets/images/shared/mobile/icon-hamburger.svg';
+
+
 const NavBar:FunctionComponent = () => {
 
-    return <>This is navbar</>
+    return (
+        <div className={styles.wrapper}>
+            <button
+                aria-label="open navigation bar"
+            >
+                <Image 
+                    src={iconHamburgerSVG}
+                    alt="Open Navigation Bar"
+                />
+            </button>
+        </div>
+    )
 }
-
 export default NavBar;
