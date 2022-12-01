2b8e23799d5a760b6dfc95ba1a2307ad2a5189fd
Minh Le <duyminh.le310@gmail.com>
2022-11-30 21:10:41 -0500

    Add Our Collection

---
+++ components/HomeComponents/OurCollection/index.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from "react";
+
+import styles from './OurCollection.module.scss';
+
+
+const OurCollection:FunctionComponent = () => {
+
+    return (
+        <div className={styles.wrapper}>
+            OurCollection
+        </div>
+    );
+}
+
+export default OurCollection;
