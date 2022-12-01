66e2ca41c28232acad970036f272b33c80dcf619
Minh Le <duyminh.le310@gmail.com>
2022-12-01 12:16:21 -0500

    Add data to WhyChooseUs

--- components/HomeComponents/WhyChooseUs/index.tsx
+++ components/HomeComponents/WhyChooseUs/index.tsx
@@ -1,11 +1,78 @@
+import Image, { StaticImageData } from "next/image";
 import { FunctionComponent } from "react";
 
+import styles from './WhyChooseUs.module.scss';
+
+import coffeeBeanSVG from '../../../assets/images/home/desktop/icon-coffee-bean.svg';
+import giftSVG from '../../../assets/images/home/desktop/icon-gift.svg';
+import truckSVG from '../../../assets/images/home/desktop/icon-truck.svg';
+
+/******************************
+ *  Types
+ */
+type ReasonData = {
+    name: string,
+    description: string,
+    image: StaticImageData
+}
+
+/******************************
+ *  Private properties
+ */
+const reasons: ReasonData[] = [
+    {
+        name: 'Best quality',
+        description: 'Discover an endless variety of the world’s best artisan coffee from each of our roasters.',
+        image: coffeeBeanSVG
+    },
+    {
+        name: 'Exclusive benefits',
+        description: 'Special offers and swag when you subscribe, including 30% off your first shipment.',
+        image: giftSVG
+    },
+    {
+        name: 'Free shipping',
+        description: 'We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.',
+        image: truckSVG
+    }
+]
+
+
+/******************************
+ *  Main Component
+ */
 const WhyChooseUs:FunctionComponent = () => {
 
     return (
-        <div>
-            Why Choose Us
-        </div>
+        <section className={styles.wrapper}>
+            <div className={styles.text}>
+                <div>  Why choose us?</div>
+                <div>A large part of our role is choosing which particular coffees will be featured 
+  in our range. This means working closely with the best coffee growers to give 
+  you a more impactful experience on every level.</div>
+            </div>
+            <div className={styles.reasons}>
+                {
+                    reasons.map(r => (
+                        <div className={styles.reason}
+                            key = {r.name}
+                        >
+                            <div className={styles.image}>
+                                <Image 
+                                    src={r.image}
+                                    alt={r.description}
+                                />
+                            </div>
+                            <div className={styles.reasonText}>
+                                <div>{r.name}</div>
+                                <div>{r.description}</div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </section>
+            
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import PageLayout from '../layouts/PageLayout';
 import HeroImage from '../components/HomeComponents/HeroImage';
 import OurCollection from '../components/HomeComponents/OurCollection';
+import WhyChooseUs from '../components/HomeComponents/WhyChooseUs';
 
 export default function Home() {
   return (
       <PageLayout title="Coffeeroasters subscription site">
         <HeroImage/>
         <OurCollection/>
+        <WhyChooseUs/>
       </PageLayout>
   )
 };
