0758f103be04667ecdb4b11c61a18a7f6f768dfb
Minh Le <duyminh.le310@gmail.com>
2022-11-30 20:12:20 -0500

    Add Home/HeroImage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import Head from 'next/head'
 
 import styles from '../styles/Home.module.scss'
 import Layout from '../layout';
+import HeroImage from '../components/HomeComponents/HeroImage';
 
 export default function Home() {
   return (
       <Layout title="Coffeeroasters subscription site">
-        main body here
+        <HeroImage/>
       </Layout>
   )
 };
