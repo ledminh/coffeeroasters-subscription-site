5ecc56c4fd0b4126f8de32e233610c29c94258c3
Minh Le <duyminh.le310@gmail.com>
2022-12-01 20:13:39 -0500

    About/OurCommitment

--- components/AboutComponents/OurCommitment/index.tsx
+++ components/AboutComponents/OurCommitment/index.tsx
@@ -1,9 +1,25 @@
 import { FunctionComponent } from "react";
 
+import styles from './OurCommitment.module.scss';
+import Image from "next/image";
+
 const OurCommitment:FunctionComponent = () => {
 
     return (
-        <>Our Commitment</>
+        <section className={styles.wrapper}>
+            <div className={styles.image}>
+                <Image 
+                    src=""
+                    alt="image"
+                />
+            </div>
+            <div className={styles.text}>
+                <div>Our commitment</div>
+                <div>
+                    We’re built on a simple mission and a commitment to doing good along the way. We want to make it easy for you to discover and brew the world’s best coffee at home. It all starts at the source. To locate the specific lots we want to purchase, we travel nearly 60 days a year trying to understand the challenges and opportunities in each of these places. We collaborate with exceptional coffee growers and empower a global community of farmers through with well above fair-trade benchmarks. We also offer training, support farm community initiatives, and invest in coffee plant science. Curating only the finest blends, we roast each lot to highlight tasting profiles distinctive to their native growing region.
+                </div>
+            </div>
+        </section>
     )
 }
 
