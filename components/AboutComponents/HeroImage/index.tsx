c1adea3400dbc4e181c34bec9bf53aba3eeba965
Minh Le <duyminh.le310@gmail.com>
2022-12-01 20:03:21 -0500

    About/HeroImage

--- components/AboutComponents/HeroImage/index.tsx
+++ components/AboutComponents/HeroImage/index.tsx
@@ -1,10 +1,37 @@
 import { FunctionComponent } from "react";
 
+import styles from './HeroImage.module.scss';
+
+import heroWhitecupJPG_mobile from '../../../assets/images/about/mobile/image-hero-whitecup.jpg';
+import heroWhitecupJPG_tablet from '../../../assets/images/about/tablet/image-hero-whitecup.jpg';
+import heroWhitecupJPG_desktop from '../../../assets/images/about/desktop/image-hero-whitecup.jpg';
+
+import HeroLayout from "../../../layouts/HeroLayout";
+
 const HeroImage:FunctionComponent = () => {
 
+
     return (
-        <>Hero Image</>
+        <HeroLayout 
+            heights={{
+                mobile: '400px',
+                tablet: '400px',
+                desktop: '450px'
+            }}
+            images={{
+                mobile: heroWhitecupJPG_mobile,
+                tablet: heroWhitecupJPG_tablet,
+                desktop: heroWhitecupJPG_desktop
+            }} 
+            altText="White Cup Hero image"
+            >
+                <div className={styles.wrapper}>
+                    <h1 className={styles.title}>About us</h1>
+                    <div>Coffeeroasters began its journey of exotic discovery in 1999, highlighting stories of coffee from around the world. We have since been dedicated to bring the perfect cup - from bean to brew - in every shipment.</div>
+                </div>
+        </HeroLayout>
     )
-}
+} 
+
 
 export default HeroImage;
