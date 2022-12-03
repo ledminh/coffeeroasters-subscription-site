a5747aa7ac25d38d77c60af4b2851877ec1e99be
Minh Le <duyminh.le310@gmail.com>
2022-12-02 20:07:36 -0500

    About/Quality

--- components/AboutComponents/Quality/index.tsx
+++ components/AboutComponents/Quality/index.tsx
@@ -1,9 +1,25 @@
 import { FunctionComponent } from "react";
 
+import styles from './Quality.module.scss';
+import Image from "next/image";
+
 const Quality:FunctionComponent = () => {
 
     return (
-        <>Quality</>
+        <div className={styles.wrapper}>
+            <div className={styles.text}>
+                <div>Uncompromising quality</div>
+                <div>
+                    Although we work with growers who pay close attention to all stages of harvest and processing, we employ, on our end, a rigorous quality control program to avoid over-roasting or baking the coffee dry. Every bag of coffee is tagged with a roast date and batch number. Our goal is to roast consistent, user-friendly coffee, so that brewing is easy and enjoyable.
+                </div>
+            </div>
+            <div className={styles.image}>
+                <Image 
+                    src=""
+                    alt=""
+                />
+            </div>
+        </div>
     )
 }
 
