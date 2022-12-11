4d012129e8dd94cdf98d0fb229d71a1800a14f1b
Minh Le <duyminh.le310@gmail.com>
2022-12-10 19:32:38 -0500

    Responsive title

--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -9,9 +9,16 @@ const Title:FunctionComponent = () => {
 
     return (
         <div className={styles.wrapper}>
-            <LogoSVG
-                viewBox="0 0 355.5 40.5"
-            />
+            <div className={styles.small}>
+                <LogoSVG
+                    viewBox="0 0 355.5 40.5"
+                />
+            </div>
+            <div className={styles.large}>
+                <LogoSVG
+                    viewBox="0 266 30.375"
+                />
+            </div>
         </div>
     )
 }
