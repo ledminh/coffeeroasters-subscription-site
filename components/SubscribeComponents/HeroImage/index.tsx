3f49dc76406e6ef53e50e40cbc1bb64f61dd8729
Minh Le <duyminh.le310@gmail.com>
2022-12-10 21:12:52 -0500

    Subscribe/HeroImage

--- components/SubscribeComponents/HeroImage/index.tsx
+++ components/SubscribeComponents/HeroImage/index.tsx
@@ -26,9 +26,11 @@ const HeroImage:FunctionComponent = () => {
             altText="White Cup Hero image"
             >
                 <div className={styles.wrapper}>
-                    <h1 className={styles.title}>Create a plan</h1>
+                    <div className={styles.content}>
+                        <h1 className={styles.title}>Create a plan</h1>
+                        <p>Build a subscription plan that best fits your needs. We offer an assortment of the best artisan coffees from around the globe delivered fresh to your door.</p>
+                    </div>
                     <div className={styles.wedge}/>
-                    <p>Build a subscription plan that best fits your needs. We offer an assortment of the best artisan coffees from around the globe delivered fresh to your door.</p>
                 </div>
         </HeroLayout>
     )
