9232118758cfd33694cbbb737230c70c8ef8310b
Minh Le <duyminh.le310@gmail.com>
2023-01-07 01:01:56 -0500

    Create account page

---
+++ pages/account.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import PageLayout from '../layouts/PageLayout';
+
+
+interface AccountProps {}
+
+const Account: FC<AccountProps> = () => (
+  <PageLayout>
+    Account Component
+  </PageLayout>
+);
+
+export default Account;
