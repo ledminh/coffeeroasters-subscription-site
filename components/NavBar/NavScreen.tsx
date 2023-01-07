30ecbd96a72c09882b078dcf7912193a8c8633da
Minh Le <duyminh.le310@gmail.com>
2023-01-07 00:51:06 -0500

    Add Login/Logout to NavScreen

--- components/NavBar/NavScreen.tsx
+++ components/NavBar/NavScreen.tsx
@@ -1,9 +1,16 @@
 import { FunctionComponent } from "react";
 import Link from "next/link";
 
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 import styles from './NavScreen.module.scss';
 
 const NavScreen:FunctionComponent = () => {
+
+    const {data: session} = useSession();
+    const {user} = session || {user: null};
+
+
 
     return (
         <div className={styles.wrapper}>
@@ -27,6 +34,29 @@ const NavScreen:FunctionComponent = () => {
                                 <h3>Create Your Plan</h3>
                             </Link>
                         </li>
+                        {
+                            user ? (
+                                <>
+                                    <li>
+                                        <Link href="/account">
+                                            <h3>Account</h3>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <button onClick={() => signOut()}>
+                                            <h3>Logout</h3>
+                                        </button>
+                                    </li>
+                                </>
+                            ) : (
+                                <li>
+                                    <button onClick={() => signIn()}>
+                                        <h3>Login</h3>
+                                    </button>
+                                </li>
+                            )
+                        }
+                        
                     </ul>
                 </nav>
             </div>
